0347b750180d9c0d367dee6cf6723a46bab90c50
mmerah <mohammedimerah@nanoleq.com>
2021-09-19 12:56:09 +0200

    Solved bad spawning of monsters

--- src/entities/room.c
+++ src/entities/room.c
@@ -125,13 +125,13 @@ void placeItems(const Room ** rooms, Item ** items, const int numberOfItems)
         /* Choose a random room */
         roomChosen = rand() % 6;
         /* Random spot in the room */
-        items[i]->position->x = (rand() % (rooms[roomChosen]->height - 2)) + rooms[roomChosen]->position.y + 1;
+        items[i]->position->x = (rand() % (rooms[roomChosen]->width - 2)) + rooms[roomChosen]->position.x + 1;
         items[i]->position->y = (rand() % (rooms[roomChosen]->height - 2)) + rooms[roomChosen]->position.y + 1;
     }
 }
 
 void setStartingPosition(Monster * monster, Room * room)
 {
-    monster->position->x = (rand() % (room->height - 2)) + room->position.y + 1;
+    monster->position->x = (rand() % (room->width - 2)) + room->position.x + 1;
     monster->position->y = (rand() % (room->height - 2)) + room->position.y + 1;
 }
